831c66ebc17733d2ce20b345f4dd428df0fc8cbc
설현오 <tjfgusdh@naver.com>
2025-07-02 12:47:47 +0900

    현오 수정

--- src/main/java/com/ndh1614/jsp_servlet_project_2505/dao/FeePolicyDAO.java
+++ src/main/java/com/ndh1614/jsp_servlet_project_2505/dao/FeePolicyDAO.java
@@ -1,4 +1,59 @@
 package com.ndh1614.jsp_servlet_project_2505.dao;
 
+import com.ndh1614.jsp_servlet_project_2505.domain.FeePolicyVO;
+import lombok.Cleanup;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeePolicyDAO {
+    public List<FeePolicyVO> selectFeePolicies() {
+        String sql = "SELECT * FROM feepolicy";
+        List<FeePolicyVO> policies = new ArrayList<>();
+        try {
+            @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+            @Cleanup PreparedStatement ps = connection.prepareStatement(sql);
+            @Cleanup ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                FeePolicyVO policy = FeePolicyVO.builder()
+                        .no(rs.getInt("no"))
+                        .timeType(rs.getString("timeType"))
+                        .baseTime(rs.getInt("baseTime"))
+                        .baseFee(rs.getInt("baseFee"))
+                        .unitTime(rs.getInt("unitTime"))
+                        .unitFee(rs.getInt("unitFee"))
+                        .dailyMaxFee(rs.getInt("dailyMaxFee"))
+                        .discountDisabled(rs.getDouble("discountDisabled"))
+                        .discountCompact(rs.getDouble("discountCompact"))
+                        .build();
+                policies.add(policy);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return policies;
+    }
+
+    public boolean insertPolicy(FeePolicyVO feePolicyVO) {
+        String sql = "INSERT INTO feepolicy (timeType, baseTime, baseFee, unitTime, unitFee, dailyMaxFee, discountDisabled, discountCompact) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try {
+            @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+            @Cleanup PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, feePolicyVO.getTimeType());
+            ps.setInt(2, feePolicyVO.getBaseTime());
+            ps.setInt(3, feePolicyVO.getBaseFee());
+            ps.setInt(4, feePolicyVO.getUnitTime());
+            ps.setInt(5, feePolicyVO.getUnitFee());
+            ps.setInt(6, feePolicyVO.getDailyMaxFee());
+            ps.setDouble(7, feePolicyVO.getDiscountDisabled());
+            ps.setDouble(8, feePolicyVO.getDiscountCompact());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
